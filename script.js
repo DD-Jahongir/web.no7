bccd640d39b12acb1257c56f54d3085a3ab25cf3
Jahongir <138986850+DD-Jahongir@users.noreply.github.com>
2024-11-20 00:06:23 +0500

    Update and rename script.js to gallery.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function calculateTotal() {
-    const price = +document.getElementById('product-select').value;
-    const quantity = +document.getElementById('quantity').value;
-    const total = price * quantity;
-    document.getElementById('order-total').textContent = total.toFixed(2);
-}
